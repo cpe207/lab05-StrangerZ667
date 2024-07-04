b1fe20f9c7461c93753b11d7e4a09b7e7d31cb8c
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-07-04 14:18:04 +0700

    V1

--- q1.ts
+++ q1.ts
@@ -1,39 +1,29 @@
-// define interface for Hero and Shop objects
-interface Hero {
-  /* Your code here */
-}
-
-interface Shop {
-  /* Your code here */
-}
-
-// assign interface/type to the function definition properly
 function buyItem(hero, shop) {
-  /* Your code here */
+    if (hero.gold >= shop.price) {
+        hero.gold -= shop.price;
+        hero.items.push(shop.item);
+    }
+    return hero;
 }
 
-//Test cases : assign proper type/interface to all objects
-const hero1 = {
-  items: ["sword", "potion"],
-  gold: 50,
+var hero1 = {
+    items: ["sword", "potion"],
+    gold: 50,
 };
-
-const shop1 = {
-  item: "armor",
-  price: 20,
+var shop1 = {
+    item: "armor",
+    price: 20,
 };
-
-const hero2 = {
-  items: ["sword", "potion"],
-  gold: 50,
+var hero2 = {
+    items: ["sword", "potion"],
+    gold: 50,
 };
-
-const shop2 = {
-  item: "legendary armor",
-  price: 200,
+var shop2 = {
+    item: "legendary armor",
+    price: 200,
 };
-
 console.log(buyItem(hero1, shop1));
 console.log(buyItem(hero2, shop2));
-
 module.exports = buyItem;
+
+//นายกรรชัย คุ้มดี 660610739
